58a959181d3d1a023d5ae70139bae721599159ca
tylerbd2022 <tyler@engajje.com>
2025-03-26 16:37:37 -0400

    Initial commit

---
+++ src/components/Experience.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+function Experience() {
+  return (
+    <div className="Experience">
+      <h1>Experience</h1>
+      <div className="experience-summary">
+        <p>
+          I am a software engineer and solutions architect with broad experince in the 
+          tech sector. 
+          I have over 5 years of experience building web applications
+          and distributed systems. I specialize in automation and AI development leveraging 
+          Microsoft Power Platform and various AI agents. I also 
+          have experience as a 
+          full-stack developer using React,
+          Node.js, and cloud technologies.
+        </p>
+      </div>
+      <div className="experience-content">
+        <h2>Work Experience</h2>
+            <h3>Fluid Solutions Group</h3>
+                <p>
+                    Automation and AI Developer
+                </p>
+                <p>
+                    2022-2025
+                </p>
+            <h3>
+                Engajje
+            </h3>
+            <p>
+                Lead Fullstack Developer
+            </p>
+            <p>2022-2025</p>
+      </div>
+    </div>
+  );
+}
+
+export default Experience; 
---
+++ src/components/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <header className="App-header">
+        <p>
+            Tyler Gruttadauria
+        </p>
+        <p className="App-subtitle">
+            Software Engineer
+        </p>
+        <a
+             className="App-link"
+             href="https://www.linkedin.com/in/tyler-g-31523b141/"
+             target="_blank"
+             rel="noopener noreferrer"
+        >
+            LinkedIn
+        </a>
+    </header>
+  );
+}
+
+export default Home;
